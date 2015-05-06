daebe7501d65302006a13a944dc6fef21316053d
Likit Preeyanon <preeyano@msu.edu>
2015-05-06 11:06:11 +0700

    added forms for login

--- webapp/app/auth/views.py
+++ webapp/app/auth/views.py
@@ -1,6 +1,20 @@
-from flask import render_template
+from flask import render_template, request, url_for, flash, redirect
+from flask.ext.login import login_user
+from ..models import Customer
+from .forms import LoginForm
 from . import auth
 
 @auth.route('/login')
 def login():
     return render_template('auth/login.html')
+
+@auth.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        customer = Customer.query.filter_by(email=form.email.data).first()
+        if customer is not None and customer.verify_password(form.password.data):
+            login_user(customer, form.remember_me.data)
+            return redirect(request.args.get('next') or url_for('main.index'))
+        flash('Invalid email or password')
+    return render_template('auth/login.html', form=form)
--- webapp/app/models.py
+++ webapp/app/models.py
@@ -5,7 +5,7 @@ from . import db
 class Customer(UserMixin, db.Model):
     __tablename__ = 'customers'
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(64), unique=True)
+    email = db.Column(db.String(64), unique=True)
 
     password_hash = db.Column(db.String(128))
 
@@ -21,7 +21,7 @@ class Customer(UserMixin, db.Model):
         return check_password_hash(self.password_hash, password)
 
     def __repr__(self):
-        return '<Customer %r>' % self.name
+        return '<Customer:email %r>' % self.email
 
 
 from . import login_manager
