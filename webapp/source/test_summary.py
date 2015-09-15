63f796e87bad195792d32591b74094c8edd2d14a
Likit Preeyanon <preeyano@msu.edu>
2015-09-15 21:35:44 +0700

    added filter_values function and did minor refactoring

--- webapp/source/test_summary.py
+++ webapp/source/test_summary.py
@@ -22,7 +22,7 @@ for m in test_methods:
 all_methods = []
 
 
-def calculate(values, principle):
+def calculate(values, principle, sd_range):
     mean = np.mean(values)
     std = np.std(values)
     print '\n' + principle
@@ -31,14 +31,35 @@ def calculate(values, principle):
     print "mean = {0:f}".format(mean)
     print "SD = {0:f}".format(std)
     print "%CV = {0:f}".format(std / mean * 100.0)
-    print "mean + 1.5SD = {0:f}".format(mean + 1.5 * std)
-    print "mean - 1.5SD = {0:f}".format(mean - 1.5 * std)
+    print "mean + {0:f}SD = {1:f}".format(sd_range, mean + 1.5 * std)
+    print "mean - {0:f}SD = {1:f}".format(sd_range, mean - 1.5 * std)
+
+
+def filter_values(values, sd_range):
+    """Returns values within std range"""
+    mean = np.mean(values)
+    std = np.std(values)
+    upper = mean + (sd_range * std)
+    lower = mean - (sd_range * std)
+    filtered_values = []
+    for val in values:
+        if upper >= val >= lower:
+            filtered_values.append(val)
+
+    return filtered_values
 
 
 for m in test_methods:
-    calculate(test_values[m], m)
+    calculate(test_values[m], m, 1.5)
     all_methods += test_values[m]
 
 principle = "All"
 
-calculate(all_methods, principle)
+calculate(all_methods, principle, 1.5)
+
+for m in test_methods:
+    filtered_values = filter_values(test_values[m], 3)
+    calculate(filtered_values, 'New ' + m, 1.5)
+
+filtered_values = filter_values(all_methods, 3)
+calculate(filtered_values, 'New all', 1.5)
