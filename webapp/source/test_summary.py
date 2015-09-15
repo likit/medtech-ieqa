bcc003dffa18b269d41c03c4e9f59c90e413c537
Likit Preeyanon <preeyano@msu.edu>
2015-09-15 20:39:47 +0700

    refactored code by adding calculate function
    added code to calculate values from all methods

--- webapp/source/test_summary.py
+++ webapp/source/test_summary.py
@@ -19,6 +19,26 @@ for m in test_methods:
             tests.append(r[test_name])
     test_values[m] = tests
 
-for m in test_methods:
-    print m, np.mean(test_values[m]), '(%d)' % len(test_values[m])
+all_methods = []
 
+
+def calculate(values, principle):
+    mean = np.mean(values)
+    std = np.std(values)
+    print '\n' + principle
+    print '-----------------'
+    print 'N = %d' % (len(values))
+    print "mean = {0:f}".format(mean)
+    print "SD = {0:f}".format(std)
+    print "%CV = {0:f}".format(std / mean * 100.0)
+    print "mean + 1.5SD = {0:f}".format(mean + 1.5 * std)
+    print "mean - 1.5SD = {0:f}".format(mean - 1.5 * std)
+
+
+for m in test_methods:
+    calculate(test_values[m], m)
+    all_methods += test_values[m]
+
+principle = "All"
+
+calculate(all_methods, principle)
